e5c61aa4b9d7ab24c386f271f6decc96f500ab1d
Przemkowsky <150743595+Przemkowsky@users.noreply.github.com>
2025-01-19 00:12:03 +0100

    Update index.js

--- index/index.js
+++ index/index.js
@@ -4,19 +4,24 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
   links.forEach(link => {
     link.addEventListener('click', function() {
-      links.forEach(link => link.classList.remove('active')); // Usuwa klasę 'active' z wszystkich linków
-      this.classList.add('active'); // Dodaje klasę 'active' do klikniętego linku
+      // Usuwa aktywność i strzałki z wszystkich linków
+      links.forEach(link => {
+        link.classList.remove('active');
+        link.classList.remove('left-arrow');
+        link.classList.remove('right-arrow');
+      });
+
+      // Dodaje klasę 'active' i strzałkę do klikniętego linku
+      this.classList.add('active');
 
       // Sprawdzenie, z której strony jest kliknięty link
       const linkPosition = this.parentElement.getBoundingClientRect().left;
       if (linkPosition < window.innerWidth / 2) {
         // Link po lewej stronie: strzałka skierowana w prawo
         this.classList.add('left-arrow');
-        this.classList.remove('right-arrow');
       } else {
         // Link po prawej stronie: strzałka skierowana w lewo
         this.classList.add('right-arrow');
-        this.classList.remove('left-arrow');
       }
     });
   });
