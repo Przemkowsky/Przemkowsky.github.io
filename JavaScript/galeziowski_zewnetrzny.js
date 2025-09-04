8152d5cb4801f00fe2f163c3a2df1591a0298275
Przemkowsky <150743595+Przemkowsky@users.noreply.github.com>
2025-09-04 08:59:44 +0200

    Add files via upload

--- JavaScript/galeziowski_zewnetrzny.js
+++ JavaScript/galeziowski_zewnetrzny.js
@@ -1,2 +1,2 @@
-document.write("ostatnia modyfikacja strony".strike() .fontcolor("yellow").bold().fontsize(7)+"<br>"); 
-document.write(document.lastModified .strike().fontcolor("yellow"). fontsize(7).bold());
+document.write("ostatnia modyfikacja strony".fontcolor("yellow").bold().fontsize(7)+"<br>"); 
+document.write(document.lastModified .fontcolor("yellow"). fontsize(7).bold());
